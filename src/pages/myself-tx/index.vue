14cb61701828783a787ab8716627d724f10e1d91
unknown <1060556811@qq.com>
2019-04-25 17:18:52 +0800

    yxm

--- src/pages/myself-tx/index.vue
+++ src/pages/myself-tx/index.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="container">
-		<div class="tit">提现到微信零钱</div>
+		<div class="tit">
+			<span>到账银行卡</span>
+			<span> 添加银行卡</span>
+			<span class="iconfont">&#xe625;</span>
+		</div>
 		<div class="txt">提现金额</div>
 		<div class="inp">
 			<span>¥</span>
@@ -8,6 +12,13 @@
 		</div>
 		<div class="name">当前可提奖金100元</div>
 		<div :class="txet==''?'btn-on':''" class="btn">提现</div>
+		
+		<!--提现成功弹窗-->		
+		<div class="pop" v-if="false">
+			<div class="iconfont img">&#xe630;</div>
+			<div class="cg">已成功提现到银行卡</div>
+			<div class="wc">完成</div>
+		</div>	
 	</div>
 </template>
 <script>
@@ -40,6 +51,17 @@
 			font-size: 15px;
 			color: #333333;
 			padding: 40px 0 60px 0;
+			span{
+				&:nth-child(2){
+					font-size: 15px;
+					color: #26ACFF;
+					margin-left: 28px;
+				}
+				&:nth-child(3){ 
+                      color:#b2e2ff;
+                      margin-left: 4px;
+				}
+			}
 		}
 		.txt {
 			font-size: 15px;
@@ -94,4 +116,44 @@
 			color: #f9f9f9;
 		}
 	}
+	/*提现成功弹窗*/
+.pop{
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	top: 0;
+	left: 0;
+	background: #FFFFFF;
+	z-index: 99;
+	.img{
+		width: 80px;
+		height: 80px;
+		background: #26acff;
+		border-radius: 50%;
+		font-size: 40px;
+		color: #FFFFFF;
+		text-align: center;
+		line-height: 80px;
+		margin: 80px auto 16px auto;
+	}
+	.cg{
+		font-size: 17px;
+		color: #333333;
+		font-weight: bold;
+		text-align: center;
+	}
+	.wc{
+		width: 160px;
+		height: 33px;
+		border-radius: 17px;
+		border: 1px solid #26ACFF;
+		margin: 0 auto;
+		text-align: center;
+		line-height: 33px;
+		color: #26acff;
+		font-size: 14px;
+		font-weight: bold;
+		margin-top: 244px;
+	}
+}
 </style>
